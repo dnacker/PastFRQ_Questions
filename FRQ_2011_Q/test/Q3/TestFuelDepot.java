8e533ff927056d07c17df8e0bd64435a16f50c3a
daackerman <daackerman@seattleschools.org>
2017-03-02 11:35:45 -0800

    updated TestFuelDepot

--- FRQ_2011_Q/test/Q3/TestFuelDepot.java
+++ FRQ_2011_Q/test/Q3/TestFuelDepot.java
@@ -12,20 +12,28 @@ import java.util.List;
  */
 public class TestFuelDepot {
     private FuelDepot depot;
-    private List<FuelTank> tanks;
+    private FuelDepot depot2;
+    private List<FuelTank> tanks1;
+    private List<FuelTank> tanks2;
 
     @Before
     public void init() {
-        tanks = new ArrayList<FuelTank>();
-        tanks.add(new Tank(20));
-        tanks.add(new Tank(30));
-        tanks.add(new Tank(80));
-        tanks.add(new Tank(55));
-        tanks.add(new Tank(50));
-        tanks.add(new Tank(75));
-        tanks.add(new Tank(20));
+        tanks1 = new ArrayList<FuelTank>();
+        tanks2 = new ArrayList<FuelTank>();
+        tanks1.add(new Tank(20));
+        tanks1.add(new Tank(30));
+        tanks1.add(new Tank(80));
+        tanks1.add(new Tank(55));
+        tanks1.add(new Tank(50));
+        tanks1.add(new Tank(75));
+        tanks1.add(new Tank(20));
         FuelRobot filler = new Robot(2, true);
-        depot = new FuelDepot(filler, tanks);
+        depot = new FuelDepot(filler, tanks1);
+
+        tanks2.add(new Tank(30));
+        tanks2.add(new Tank(20));
+        tanks2.add(new Tank(40));
+        depot2 = new FuelDepot(filler, tanks2);
     }
 
     @Test
@@ -39,9 +47,14 @@ public class TestFuelDepot {
     }
 
     @Test
+    public void testNextTankToFillNotLast() {
+        Assert.assertEquals(1, depot2.nextTankToFill(50));
+    }
+
+    @Test
     public void testMoveRightForward() {
         FuelRobot filler = new Robot(2, true);
-        FuelDepot depot1 = new FuelDepot(filler, tanks);
+        FuelDepot depot1 = new FuelDepot(filler, tanks1);
         depot1.moveToLocation(4);
         Assert.assertEquals(4, filler.getCurrentIndex());
     }
@@ -49,7 +62,7 @@ public class TestFuelDepot {
     @Test
     public void testMoveRightBackward() {
         FuelRobot filler = new Robot(2, false);
-        FuelDepot depot1 = new FuelDepot(filler, tanks);
+        FuelDepot depot1 = new FuelDepot(filler, tanks1);
         depot1.moveToLocation(4);
         Assert.assertEquals(4, filler.getCurrentIndex());
     }
@@ -57,7 +70,7 @@ public class TestFuelDepot {
     @Test
     public void testMoveLeftBackward() {
         FuelRobot filler = new Robot(4, true);
-        FuelDepot depot1 = new FuelDepot(filler, tanks);
+        FuelDepot depot1 = new FuelDepot(filler, tanks1);
         depot1.moveToLocation(2);
         Assert.assertEquals(2, filler.getCurrentIndex());
     }
@@ -65,7 +78,7 @@ public class TestFuelDepot {
     @Test
     public void testMoveLeftForward() {
         FuelRobot filler = new Robot(4, false);
-        FuelDepot depot1 = new FuelDepot(filler, tanks);
+        FuelDepot depot1 = new FuelDepot(filler, tanks1);
         depot1.moveToLocation(2);
         Assert.assertEquals(2, filler.getCurrentIndex());
     }
